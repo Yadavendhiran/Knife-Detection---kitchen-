efc745d7c19d881fb1747b3885735b52510ab450
Yadavendhiran <oddashettyyadu@gmail.com>
2026-03-14 16:15:07 +0530

    Added alarm sound when knife has been detected

--- tp_cv.py
+++ tp_cv.py
@@ -1,5 +1,7 @@
 import cv2
 from ultralytics import YOLO
+from playsound import playsound
+import threading
 
 model = YOLO(r"D:\projects\runs\detect\train5\weights\best.pt")
 print("model loaded")
@@ -18,8 +20,20 @@ while True:
 
     results = model(frame)
     annoted_frame=results[0].plot()
-    # print(results)
     cv2.imshow("Knife Detected",annoted_frame)
+    # print(results)
+    if results[0].boxes is not None:
+        classes=results[0].boxes.cls.tolist()
+
+        #--Thread to isolated or not distrub the camera
+        if 2 in classes:
+            threading.Thread(
+                target=playsound,
+                args=(r"A:\rec\gif\audio\alarm.wav",),
+                daemon=True
+            ).start()
+
+        # playsound(r"A:\rec\gif\audio\alarm.wav") to play in sync and not in thread
 
     if cv2.waitKey(1)==27:
         break
