fce062c9e1b506c283e9c0e2bd0db7a87f00c30d
Yadavendhiran <oddashettyyadu@gmail.com>
2026-04-13 23:11:41 +0530

    Added encoding

--- llm.py
+++ llm.py
@@ -1,8 +1,28 @@
-text="some string string"
-token=list(set(text.split()))
-# word2idx={}
-# for w,i in enumerate(token):
-#     word2idx.update({w:i})
+text="You sleep in the bed that you make not in the one that was made"
+token=sorted(set(text.split()))
+word2idx={}
 
+#  create encoding set for each word and then reverse encoding
+for i,w in enumerate(token):
+    word2idx.update({w:i})
+idx2wrd={}
+for i,w in enumerate(token):
+    idx2wrd.update({i:w})
+print(word2idx)
+# sentence encoding
+encode=[]
+for w in text.split():
+    encode.append(word2idx[w])
+
+
+# create tensor dataset for pytorch model
+import torch
+x,y=[],[]
+for i in range(len(encode)-1):
+    x.append(encode[i])
+    y.append(encode[i+1])
+x=torch.tensor(x)
+y=torch.tensor(y)
 
 print(token)
+print(encode)
